e7d056830d9c7ef40a56c65aee55dd816ccf54af
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-10 02:09:59 +0530

    pagination

--- app.js
+++ app.js
@@ -41,6 +41,35 @@ router.get('/usersdata',(req,res) => {
   })
 
 
+// http://localhost:3000/api/usersdata?pageNo=1&size=5
+
+  router.get('/users',(req,res) => {
+    var pageNo = parseInt(req.query.pageNo)
+    var size = parseInt(req.query.size)
+    var query = {}
+    if(pageNo < 0 || pageNo === 0) {
+          response = {"error" : true,"message" : "invalid page number, should start with 1"};
+          return res.json(response)
+    }
+    query.skip = size * (pageNo - 1)
+    query.limit = size
+    // Find some documents
+    users.count({},function(err,totalCount) {
+               if(err) {
+                 response = {"error" : true,"message" : "Error fetching data"}
+               }
+               users.find({},{},query,function(err,data) {
+                // Mongo command to fetch all data from collection.
+              if(err) {
+                  response = {"error" : true,"message" : "Error fetching data"};
+              } else {
+                  var totalPages = Math.ceil(totalCount / size)
+                  response = {"error" : false,"message" : data,"pages": totalPages};
+              }
+              res.json(response);
+           });
+         })
+  })
 
 
 
