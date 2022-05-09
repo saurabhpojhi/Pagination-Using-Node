2287edf2ec402e02b2d9da20b8c1d0f37846f1aa
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-10 01:59:51 +0530

    pagination

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const users = require('./models/users');
 const app = express()
 const router = express.Router()
 
+const port = 3000;
+
+// connnect database 
+mongoose.connect('mongodb://localhost:27017/Pagination_Test')
+.then(conn=>{
+    console.log('MongoDB connected')
+})
+.catch(error=>{
+    console.log("Error:"+ error.Message)
+})
 
 
 
 
 
+
+
+//server.listen(port);
+app.listen((port),()=>{
+    console.log(`Server is Running ${port}`)
+});
 
 
 app.use('/api',router)
+
+module.exports =app;
