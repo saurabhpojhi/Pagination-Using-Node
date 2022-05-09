ae70644afd1161f50f843bbbbc5698ec148e9e2f
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-10 02:06:38 +0530

    pagination

--- app.js
+++ app.js
@@ -16,6 +16,29 @@ mongoose.connect('mongodb://localhost:27017/Pagination_Test')
 })
 
 
+// http://localhost:3000/api/usersdata?pageNo=1&size=10
+
+router.get('/usersdata',(req,res) => {
+    var pageNo = parseInt(req.query.pageNo)
+    var size = parseInt(req.query.size)
+    var query = {}
+    if(pageNo < 0 || pageNo === 0) {
+          response = {"error" : true,"message" : "invalid page number, should start with 1"};
+          return res.json(response)
+    }
+    query.skip = size * (pageNo - 1)
+    query.limit = size
+    // Find some documents
+         users.find({},{},query,function(err,data) {
+          // Mongo command to fetch all data from collection.
+              if(err) {
+                  response = {"error" : true,"message" : "Error fetching data"};
+              } else {
+                  response = {"error" : false,"message" : data};
+              }
+              res.json(response);
+          });
+  })
 
 
 
